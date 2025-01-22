d01be3ccf59be335da0390286cf1080a39013486
Mariia Poliak <mariiapoliak1997@gmail.com>
2025-01-22 02:21:12 +0100

    tyu

--- src/js/header.js
+++ src/js/header.js
@@ -1,3 +1,23 @@
+// Функція для зміни зображення і стану меню
+const toggleMenu = () => {
+  const isExpanded = header.classList.toggle('expanded');
+  navigation.classList.toggle('expanded');
+  mobileMenu.classList.toggle('expanded');
+  mobilePolice.classList.toggle('expanded');
+
+  menuIcon.src = isExpanded ? './image/close.png' : './image/open.png';
+};
+
+// Функція для закриття меню
+const closeMenu = () => {
+  header.classList.remove('expanded');
+  navigation.classList.remove('expanded');
+  mobileMenu.classList.remove('expanded');
+  mobilePolice.classList.remove('expanded');
+  menuIcon.src = './image/close.png';
+};
+
+// Вибір елементів
 const header = document.querySelector('.header');
 const navigation = document.querySelector('.nav-container');
 const menuToggle = document.querySelector('.menu-toggle');
@@ -6,39 +26,25 @@ const mobileMenu = document.querySelector('.nav-menu-mobile');
 const mobilePolice = document.querySelector('.police-mobile');
 const navLinks = document.querySelectorAll('.nav-link');
 
-menuToggle.addEventListener('click', () => {
-  const isExpanded = header.classList.toggle('expanded');
-  navigation.classList.toggle('expanded');
-  mobileMenu.classList.toggle('expanded');
-  mobilePolice.classList.toggle('expanded');
+// Додавання слухача події для кнопки меню
+menuToggle.addEventListener('click', toggleMenu);
 
-  menuIcon.src = isExpanded ? './image/close.png' : './image/open.png';
-});
+// Додавання слухачів події для кожного посилання
+navLinks.forEach(link => link.addEventListener('click', closeMenu));
 
-navLinks.forEach(link => {
-  link.addEventListener('click', () => {
-    header.classList.remove('expanded');
-    navigation.classList.remove('expanded');
-    mobileMenu.classList.remove('expanded');
-    mobilePolice.classList.remove('expanded');
-    menuIcon.src = './image/close.png';
-  });
-});
-
+// Обробка плавної прокрутки
 document.addEventListener('DOMContentLoaded', () => {
   const headerHeight = document.querySelector('.header').offsetHeight;
 
   document.querySelectorAll('a.nav-link[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
       e.preventDefault();
-
       const targetId = this.getAttribute('href').slice(1);
       const targetElement = document.getElementById(targetId);
 
       if (targetElement) {
         const targetPosition =
           targetElement.getBoundingClientRect().top + window.scrollY;
-
         window.scrollTo({
           top: targetPosition - (headerHeight + 40),
           behavior: 'smooth',
@@ -47,3 +53,6 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   });
 });
+
+// Логування для перевірки правильності елементів
+console.log(menuToggle, menuIcon);
